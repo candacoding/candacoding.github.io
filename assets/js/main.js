874c35d889127b710f8701a3ce040e5e96be6aca
candacoding <hfzrifani@gmail.com>
2022-11-18 18:33:49 +0700

    Version 1.3

---
+++ assets/js/main.js
@@ -0,0 +1,1 @@
+$(window).load(function() { $("#loading").delay(1000).fadeOut("slow"); } )
--- assets/js/script.js
+++ assets/js/script.js
@@ -14,3 +14,5 @@ if (btn !== null) {
     bar.classList.toggle('fa-times');
   });
 }
+
+$(window).load(function() { $("#loading").delay(1000).fadeOut("slow"); } )
